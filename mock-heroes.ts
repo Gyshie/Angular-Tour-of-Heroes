237857ff8b7881d3204b268e1ae698b2106063e8
Gayasha Malluwawadu <36964262+Gyshie@users.noreply.github.com>
2018-11-18 13:45:18 +0530

    Add files via upload

---
+++ mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Juggernaut' },
+  { id: 2, name: 'Ursa' },
+  { id: 3, name: 'Bloodseeker' },
+  { id: 4, name: 'Invoker' },
+  { id: 5, name: 'Magnus' },
+  { id: 6, name: 'Storm Spirit' },
+  { id: 7, name: 'Bane' },
+  { id: 8, name: 'Enigma' },
+  { id: 9, name: 'Viper' },
+  { id: 10, name: 'Clockwerk' }
+];
